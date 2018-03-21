ccebda5f2a094c83eb9c0ea3a85e31cae2e333c1
Balaji <balaji.jayasankar@gmail.com>
2018-03-21 23:03:13 +0530

    front end for search by status

--- src/searchStatus.php
+++ src/searchStatus.php
@@ -49,7 +49,7 @@ input[type=text], select, textarea{
 
 
 <body>
-    <a href="logout.php"><img src="123.png" height="50px" width="50px"></a>
+    <a href="adminhome.php"><img src="back.png" height="50px" width="50px"></a>
 <center>
 <h1>NATIONAL INSTITUTE OF TECHNOLOGY CALICUT</h1>
 <h2>phD Student Project Management System</h2></center>
@@ -58,36 +58,10 @@ input[type=text], select, textarea{
 <center><button onclick="window.location.href='add.php'">Add New Project</button></center>
 <br><br>
 
-<form action="search.php" method="POST">
-<input type="text" name="search" placeholder="Search by RollNo or Name">
-<input type="submit" value="search">
-</form>
-<form action="searchStatus.php" method="POST">
-<label >Search by status</label>
-<select  id = 'status' name="status">
-<option value="">Select the option</option>    
-<option value="option1">option1</option>
-<option value="option2">option2</option>
-<option value="option3">option3</option>
-<option value="option4">option4</option>
-<option value="option5">option5</option>
-<option value="option6">option6</option>
-<option value="option7">option7</option>
-<option value="option8">option8</option>
-<option value="option9">option9</option>
-<option value="option10">option10</option>
-</select>
-<br>
-<input type="submit" value="search">
-</form>
-
-
 <?php
 
 
-
 $roll="";
-    
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $servername = $url["host"];
@@ -97,21 +71,28 @@ $db = substr($url["path"], 1);
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
 
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-
+$search=mysqli_real_escape_string($conn, $_POST['search']);
 
 
 $sql = "SELECT name,project.rollno,email,department,guide,guidemail,topic,status,date
 FROM project,mtechstudent 
 where project.rollno=mtechstudent.rollno";
+
 $result= mysqli_query($conn, $sql);
 $availability=0;
 
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
+    
+    if($row["rollno"]==$search ||$row["name"]==$search)
+      {
+
+
               
              $availability=1;
               $roll=$row['rollno'];
@@ -129,12 +110,6 @@ if (mysqli_num_rows($result) > 0) {
   <input type="hidden" name="rollno" value=<?php echo $roll ?> >
   <input type="submit" value="Edit">
 </form>
-<br>
- <form action="history.php" method="post" >
-  <input type="hidden" name="rollno" value=<?php echo $roll ?> >
-  <input type="submit" value="History">
-</form>
-
 
 </center>
 </div>
@@ -142,7 +117,7 @@ if (mysqli_num_rows($result) > 0) {
 <?php
 }
 }
-
+}
 
 
 if($availability==0)
@@ -180,6 +155,3 @@ mysqli_close($conn);
 </html>
 
 
-
-</body>
-</html>
